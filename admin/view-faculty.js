c45e03ed306aff62153d3d3bd95f2b3e223281d6
Bhagat Singh <bhagatsingh.17is@saividya.ac.in>
2020-04-18 16:39:23 +0530

    student and faculty update delete done

--- admin/view-faculty.js
+++ admin/view-faculty.js
@@ -1,25 +1,72 @@
+let field1, field2, result, allDataObj;
+
 const allFacultyRef = firebase.database().ref('addFaculty');
 
 allFacultyRef.on('value', data, error);
 
 function data(snapshot) {
-  console.log('values are');
-
-  const allDataObj = snapshot.val();
+  allDataObj = snapshot.val();
   // console.log(allDataObj);
+
+  result = Object.keys(allDataObj);
+
   var allDataArr = Object.entries(allDataObj);
   // console.log(allDataArr);
-  
+
+  $('#tableBody').empty();
   for(var i=0; i < allDataArr.length; i++) {
     let name = allDataArr[i][1].name;
     let email = allDataArr[i][1].email;
-    
+
     $('#tableBody').append(
-      '<tr>      <td>' + name + '</td>      <td>' + email + '</td>      <td>        <button class="btn btn-danger btn-sm">          <i class="fa fa-trash"></i>        </button>        <button class="btn btn-sm btn-info" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal"><i class="fa fa-wrench"></i></button>      </td>    </tr>'
+      '<tr>      <td>' + name + '</td>      <td>' + email + '</td>      <td>        <button class="btn btn-danger btn-sm"  onclick="tableValues(); deleteRecord()">          <i class="fa fa-trash"></i>        </button>        <button class="btn btn-sm btn-info" class="btn btn-info btn-lg" onclick="tableValues()" data-toggle="modal" data-target="#myModal"><i class="fa fa-wrench"></i></button>      </td>    </tr>'
     );
   }
 }
 
 function error(err) {
   console.log(err);
+}
+
+function tableValues() {
+  $('#tableBody').find('tr').click(function(){
+    field1 =$(this).find('td:first').text();
+    field2 =$(this).find('td').eq(1).text();
+    // console.log(field1, field2);
+  });
+}
+
+function deleteRecord() {
+  console.log('Deleting Record');
+  setTimeout(function () {
+    for(var i = 0; i < result.length; i++) {
+      var k = result[i];
+      var matchField1 = allDataObj[k].name;
+      var matchField2 = allDataObj[k].email;
+      if(field1 == matchField1 && field2 == matchField2 ) {
+        allFacultyRef.child(k).remove();
+      }
+    }
+  },500);
+}
+
+function upadateRecord() {
+  console.log('Updating Record');
+  for(var i = 0; i < result.length; i++) {
+    var k=result[i];
+    var matchField1 = allDataObj[k].name;
+    var matchField2 = allDataObj[k].email;
+    // console.log(matchField1, matchField2);
+
+    if((field1 == matchField1) && (field2 == matchField2)) {
+      name = document.querySelector('#name').value;
+      email = document.querySelector('#email').value;
+      updatedData = {
+        name : name,
+        email : email
+      };
+      allFacultyRef.child(k).update(updatedData);
+      // console.log("Record Updated");
+    }
+  }
 }
--- admin/view-student.js
+++ admin/view-student.js
@@ -1,24 +1,35 @@
+// gloval varaiables
+let field1, field2, field3, field4, result, allDataObj;
+
 const allStudentRef = firebase.database().ref('addStudent');
 
 allStudentRef.on('value', data, error);
 let totalRecords = 0;
 
 function data(snapshot) {
-  const allDataObj = snapshot.val();
+  allDataObj = snapshot.val();
+  // console.log(allDataObj);
+
+  // extrating data through keys
+  result = Object.keys(allDataObj);
+  // gives us an array of all the uniqueID
+  // console.log(result);
+
   // console.log(allDataObj);
   var allDataArr = Object.entries(allDataObj);
   // console.log(allDataArr);
   totalRecords = allDataArr.length;
 
+  $('#tableBody').empty();
   for(var i=0; i < allDataArr.length; i++) {
     let branch = allDataArr[i][1].branch;
     let semester = allDataArr[i][1].semester;
     let name = allDataArr[i][1].name;
     let usn = allDataArr[i][1].usn;
     let uid = allDataArr[i][0];
-    
+
     $('#tableBody').append(
-      '<tr><td>' + name + '</td>        <td>' + semester + '</td>        <td>' + branch + '</td>        <td>' + usn + '</td>        <td>          <button class="btn btn-danger btn-sm"   >            <i class="fa fa-trash"></i>          </button>          <button class="btn btn-sm btn-info" class="btn btn-info btn-lg" data-toggle="modal" id="updateBtn" value="'+uid+'"  data-target="#myModal">            <i class="fa fa-wrench"></i>        </button>        </td>     </tr>'
+      '<tr><td>' + name + '</td>        <td>' + semester + '</td>        <td>' + branch + '</td>        <td>' + usn + '</td>        <td>          <button class="btn btn-danger btn-sm"  onclick="tableValues(); deleteRecord()" >            <i class="fa fa-trash"></i>          </button>          <button class="btn btn-sm btn-info" class="btn btn-info btn-lg" data-toggle="modal" onclick="tableValues()" id="updateBtn"  data-target="#myModal">            <i class="fa fa-wrench"></i>        </button>        </td>     </tr>'
     );
   }
 }
@@ -27,54 +38,55 @@ function error(err) {
   console.log(err);
 }
 
-function impFun(){
-  console.log('hello');
-  
-  var updateBtn = document.getElementById('updateBtn');
-  updateBtn.addEventListener('click', updateForm);
-  function updateForm() {
-    const v = updating.value;
-    console.log(v);
-    console.log('clicked');
+function tableValues() {
+  $('#tableBody').find('tr').click(function(){
+    field1 =$(this).find('td:first').text();
+    field2 =$(this).find('td').eq(1).text();
+    field3 =$(this).find('td').eq(2).text();
+    field4 =$(this).find('td').eq(3).text();
+    // console.log(field1, field2, field3, field4);
+  });
+}
 
+function deleteRecord() {
+  console.log('Deleting Record');
+  setTimeout(function () {
+    for(var i = 0; i < result.length; i++) {
+      var k = result[i];
+      var matchField1 = allDataObj[k].name;
+      var matchField2 = allDataObj[k].semester;
+      if(field1 == matchField1 && field2 == matchField2 ) {
+        allStudentRef.child(k).remove();
+      }
+    }
+  },500);
+}
+
+function upadateRecord() {
+  console.log('Updating Record');
+  for(var i = 0; i < result.length; i++) {
+    var k=result[i];
+    var matchField1 = allDataObj[k].name;
+    var matchField2 = allDataObj[k].semester;
+    var matchField3 = allDataObj[k].branch;
+    var matchField4 = allDataObj[k].usn;
+    // console.log(matchField1, matchField2, matchField3, matchField4);
+
+    if((field1 == matchField1) && (field2 == matchField2) && (field3 == matchField3) && (field4 == matchField4) ) {
+      name = document.querySelector('#name').value;
+      semester = document.querySelector('#semester').value;
+      branch = document.querySelector('#branch').value;
+      usn = document.querySelector('#usn').value;
+      email = document.querySelector('#email').value;
+      updatedData = {
+        name : name,
+        semester : semester,
+        branch : branch,
+        usn : usn,
+        email : email
+      };
+      allStudentRef.child(k).update(updatedData);
+      // console.log("Record Updated");
+    }
   }
 }
-window.onload= impFun;
-
-
-// function update() {
-//   console.log('update btn clicked');
-
-//   firebase.database().ref('addStudent').then(h => {
-//     console.log(h);
-    
-//   })
-
-
-  // for(let i = 0; i < totalRecords; i++) {
-  //   let id = 'updateBtn_'+i;
-  //   console.log(id);
-    
-  //   document.getElementById(id).addEventListener('hover',doSomething);
-
-  //   function doSomething() {
-  //     console.log('hey');
-      
-  //   }
-  // }
-  // const ref = firebase.database().ref('addStudent');
-  // console.log(ref);
-  
-  // ref.on('value',lol,error);
-// }
-
-
-
-// function lol(snap) {
-//   const data = snap.val();
-//   console.log(data);
-//   const keys = Object.keys(data);
-//   console.log(keys);
-  
-  
-// }
